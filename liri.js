25fc5725a3f39bc30bf775a90a1f368d66beadd7
acarlie <ameliacarlie@gmail.com>
2019-08-28 16:07:13 -0400

    Added Bands in Town API and logic

--- liri.js
+++ liri.js
@@ -1,6 +1,7 @@
 "use strict";
 
 var axios = require('axios');
+var moment = require('moment');
 
 var app = {
     method: process.argv[2],
@@ -9,36 +10,85 @@ var app = {
             case "movie-this":
                 app.movie();
                 break;
+            case "concert-this":
+                app.bands();
+                break;
             case "spotify-this-song":
                 app.spotify();
+                break;
+            case "help":
+            default:
+                app.help();
                 break;
         }
     },
     movie(){
-        let movie = this.getArgs();
+
+        let movie = this.getArgs() ? this.getArgs() : 'Mr. Nobody';
         let queryUrl = `http://www.omdbapi.com/?t=${movie}&y=&plot=short&apikey=trilogy`;
+
         axios.get(queryUrl).then(function(response){
             let d = response.data;
-            let output = [
-                `Title:  ${d.Title}`, 
-                `Release Year: ${d.Year}`, 
-                `IMDB Rating: ${d.imdbRating}`, 
-                `Rotten Tomatoes Score: ${d.Ratings[1] !== undefined ? d.Ratings[1].Value : 'N/A'}`, 
-                `Country: ${d.Country}`, 
-                `Language(s): ${d.Language}`, 
-                `Plot: ${d.Plot}`, 
-                `Actors: ${d.Actors}`
-            ];
+            if (d.Response === 'True'){
+                let output = [
+                    `Title:  ${d.Title}`, 
+                    `Release Year: ${d.Year}`, 
+                    `IMDB Rating: ${d.imdbRating}`, 
+                    `Rotten Tomatoes Score: ${d.Ratings[1] !== undefined ? d.Ratings[1].Value : 'N/A'}`, 
+                    `Country: ${d.Country}`, 
+                    `Language(s): ${d.Language}`, 
+                    `Plot: ${d.Plot}`, 
+                    `Actors: ${d.Actors}`
+                ];
+    
+                app.consoleLog(output);
+            } else {
+                console.log(`The movie '${movie}' was not found :-(.`);
+            }
+     
+        });
+    },
+    bands(){
+        let artist = this.getArgs() ? this.getArgs() : '';
+        let queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
 
-            app.consoleLog(output);
+        axios.get(queryUrl).then(function(response){
+            let d = response.data;
+            if (response.data.length > 0){
+                app.consoleLog(['', `${artist}'s upcoming events:`, '']);
+
+               
+                let event;
+                for (event of d){
+                    let time = moment(event.datetime, 'YYYY-MM-DD[T]HH:mm:ss').format('MM/DD/YYYY');
+                    console.log(`${time} @ ${event.venue.name} in ${event.venue.city}${event.venue.region.length > 0 ? ', ' + event.venue.region : ''}`);
+                }
+                
+                console.log('');
+            }
         });
     },
     spotify(s){
 
     },
+    help(){
+        let helpArr = [
+            '',
+            '----------Liri Instructions----------',
+            '',
+            "**OMDB**",
+            "To search OMDB enter 'node liri movie-this ' and the movie name.",
+            "Ex: 'node liri movie-this die hard'",
+            '',
+            "**Bands in Town**",
+            "To search Bands in Town enter 'node liri concert-this ' and the band or artist's name.",
+            "Ex: 'node liri concert-this the black keys'",
+        ]
+        this.consoleLog(helpArr);
+    },
     getArgs(){
         const [,,,...argsArr] = process.argv;
-        return argsArr.join(" ");
+        return process.argv.length > 0 ? argsArr.join(" ") : false;
     },
     consoleLog(arr){
         let x;
@@ -49,24 +99,3 @@ var app = {
 }
 
 app.liri();
-
-// . `node liri.js movie-this '<movie name here>'`
-
-//    * This will output the following information to your terminal/bash window:
-
-//      ```
-//        * Title of the movie.
-//        * Year the movie came out.
-//        * IMDB Rating of the movie.
-//        * Rotten Tomatoes Rating of the movie.
-//        * Country where the movie was produced.
-//        * Language of the movie.
-//        * Plot of the movie.
-//        * Actors in the movie.
-//      ```
-
-//    * If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
-
-//      * If you haven't watched "Mr. Nobody," then you should: <http://www.imdb.com/title/tt0485947/>
-
-//      * It's on Netflix!
